48deba5f2cdc547ef8567eeccacadf8201229af1
Itay Plavin <4023066+itayplav@users.noreply.github.com>
2024-09-06 01:49:12 -0500

    Removed redundant `/v1/stats/*` structs from `types.go`

--- src/client/types.go
+++ src/client/types.go
@@ -1,32 +1,9 @@
 package client
 
-import (
-	"time"
-)
-
 
 
 type StonfiClientOptions struct {
 	BaseURL string
-}
-
-
-
-type DexStatsResponse struct {
-	TVL        string `json:"tvl"`
-	Volume24h  string `json:"volume_24h"`
-	TotalSwaps int    `json:"total_swaps"`
-}
-
-type OperationStatsResponse struct {
-	Operations []Operation `json:"operations"`
-}
-
-type Operation struct {
-	Type      string    `json:"type"`
-	Timestamp time.Time `json:"timestamp"`
-	Value     string    `json:"value"`
-	Status    string    `json:"status"`
 }
 
 type MarketListResponse struct {
